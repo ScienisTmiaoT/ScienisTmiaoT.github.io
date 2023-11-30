255f454f50e73655b7407b61ab4e247bcac6f607
Wenbin Guo <ScienisTmiaoT@users.noreply.github.com>
2023-11-30 10:43:46 -0600

    update

--- docs/assets/js/my_interceptor.js
+++ docs/assets/js/my_interceptor.js
@@ -1,11 +1,19 @@
 (function Interceptor(nativeOpenWrapper, nativeSendWrapper) {
     XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
-        nativeOpenWrapper.call(this, method, replaceUrl(url), async, user, password);
+        nativeOpenWrapper.call(this, method, replaceUrls(url), async, user, password);
     };
 })(XMLHttpRequest.prototype.open, XMLHttpRequest.prototype.send);
 
-function replaceUrl(url) {
-    const index = url.indexOf('/ghost/api/content/posts');
+function replaceUrls(url) {
+    const suffixes = ['posts', 'authors', 'tags'];
+    for (let i of suffixes) {
+        url = replaceUrl(url, i);
+    }
+    return url;
+}
+
+function replaceUrl(url, suffix) {
+    const index = url.indexOf('/ghost/api/content/' + suffix);
     if (index !== -1) {
         const i = url.indexOf('?');
         const new_url = url.substring(0, i) + '1.json';
